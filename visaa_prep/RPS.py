6c0e8bdef9d59362269afedfb785754431bb007b
kadaridivya <148632757+kadaridivya@users.noreply.github.com>
2024-11-15 14:27:27 +0530

    Create RPS.py

---
+++ visaa_prep/RPS.py
@@ -0,0 +1,12 @@
+Vignesh,Charan = input().split(' ')
+if(Vignesh=='S' and Charan=='P'):
+    print("Vignesh")
+elif(Vignesh=='P' and Charan=='R'):
+    print("Vignesh")
+elif(Vignesh=='R'and Charan =='S'):
+    print("Vignesh")
+elif(Vignesh==Charan):
+    print("NULL")
+else:
+    print("Charan")
+    

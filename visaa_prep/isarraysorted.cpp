1209708cd8dcf184058a9eca6e2e9d1f9adfd09f
kadaridivya <148632757+kadaridivya@users.noreply.github.com>
2024-11-16 22:37:05 +0530

    Create isarraysorted.cpp

---
+++ visaa_prep/isarraysorted.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            cout<<"false"<<endl;
+            return 0;
+        }
+    }
+    cout<<"true";
+}

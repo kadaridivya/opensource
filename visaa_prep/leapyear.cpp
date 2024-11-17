90ae9a16832da4e76af185506f350043ff6b4fb9
kadaridivya <148632757+kadaridivya@users.noreply.github.com>
2024-11-17 10:41:34 +0530

    Create leapyear.cpp

---
+++ visaa_prep/leapyear.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    if((n%400==0)||(n%4==0 && n%100!=0)){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}

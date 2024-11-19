23fed0ec7f243a169b3be4e54d9dafb94c5f5652
kadaridivya <148632757+kadaridivya@users.noreply.github.com>
2024-11-19 19:43:38 +0530

    Create absencestreak.cpp

---
+++ visaa_prep/absencestreak.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int count=0;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
